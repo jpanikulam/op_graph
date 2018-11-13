484918a823af2ff085249e16fb8c25171401c3c1
Jacob Panikulam <jpanikul@gmail.com>
2018-11-13 00:35:09 -0500

    Generating nearly acceptable C++ for graphs and subgraphs

--- op_graph/graph_to_cc.py
+++ op_graph/graph_to_cc.py
@@ -53,13 +53,19 @@ def legalize_it(sym_name):
     return new_name
 
 
+def sym_to_text(sym, gr):
+    if sym in gr.uniques:
+        return '({})'.format(sym_children_to_cc(sym, gr))
+    return str(sym)
+
+
 def binary(sym, gr, operator=None):
     assert operator is not None
     op = gr.adj[sym]
     args = graph.get_args(op)
     return "{a} {operator} {b}".format(
-        a=args[0],
-        b=args[1],
+        a=sym_to_text(args[0], gr),
+        b=sym_to_text(args[1], gr),
         operator=operator,
     )
 
@@ -67,21 +73,26 @@ def binary(sym, gr, operator=None):
 def func(sym, gr):
     op = gr.adj[sym]
     args = graph.get_args(op)
-    return "{}({})".format(op[0], ', '.join(map(str, args)))
+    return "{}({})".format(op[0], ', '.join(map(partial(sym_to_text, gr=gr), args)))
+
+
+def extract(sym, gr):
+    op = gr.adj[sym]
+    args = graph.get_args(op)
+    assert isinstance(args[1], int), "Oh shit what are you doing"
+    struct_name = args[0]
+    return "{}.{}".format(struct_name, sym)
 
 
 def build_struct(sym, gr):
     op = gr.adj[sym]
     args = graph.get_args(op)
     props = gr.properties[sym]
-    # return "{}" + "{" + "{}".format(op[0], ', '.join(args))
 
     if props['inherent_type'] is None:
         Log.warn("{} lacks inherent type".format(sym))
     assert props['inherent_type'] is not None, "Can't create type for a group without inherent type."
-
-    new_args = ',\n'.join(args)
-
+    new_args = ',\n'.join(map(partial(sym_to_text, gr=gr), args))
     return props['inherent_type'] + "{" + new_args + "}\n"
 
 
@@ -90,9 +101,9 @@ def inv(sym, gr):
     args = graph.get_args(op)
     arg_type = gr.properties[args[0]]
     if arg_type == 'liegroup':
-        return "{}.inverse()".format(args[0])
+        return "{}.inverse()".format(sym_to_text(args[0], gr))
     else:
-        return "(1.0 / {})".format(args[0])
+        return "(1.0 / {})".format(sym_to_text(args[0], gr))
 
 
 def sym_children_to_cc(sym, gr):
@@ -102,45 +113,80 @@ def sym_children_to_cc(sym, gr):
         'exp': func,
         'log': func,
         'groupify': build_struct,
+        'extract': extract,
         'inv': inv,
         'I': lambda sym, gr: graph.get_args(gr.adj[sym])[0],
     }
 
     op = gr.adj[sym]
-    # result = dispatch[op[0]](sym, gr)
     result = dispatch.get(op[0], func)(sym, gr)
 
     return result
 
 
+
+def group_to_struct(gr, group_sym, names):
+    props = gr.properties[group_sym]
+    inherent_type = props['inherent_type']
+    assert inherent_type is not None
+
+    lvalues = []
+    for _type, name in zip(props['elements'], names):
+        cc_type = to_cpp_type(_type)
+        lvalues.append(create.create_lvalue(cc_type, name))
+
+
+    mystruct = create.create_struct(
+        inherent_type,
+        lvalues
+    )
+    return mystruct
+
+    cg = CodeGraph()
+    cg.add_child(mystruct)
+
+    # myfunc = create.create_function(
+    #     'perforate',
+    #     [
+    #         create.create_lvalue('double', 'goomba'),
+    #         create.create_lvalue('Fromp&', 'goomba2'),
+    #     ],
+    #     'double'
+    # )
+
+
 def express(gr):
+
+    # for group in
+
+    for name, subgraph in gr.subgraph_functions():
+        print name + "()"
+        express(subgraph['graph'])
+        print '----'
+
     gr_sorted = graph_tools.topological_sort(gr.adj)
     inputs = graph.get_inputs(gr)
-    print 'In:', inputs
-    print gr.to_optimize
-
-    print gr.how_do_i_compute('Qn')
-
     cb = generate.CodeBlock()
 
     structs = gr.groups()
-    for struct in structs:
-        print struct
+    # for struct in structs:
+        # nstruct = group_to_struct(gr, struct, )
+        # generate.generate_struct(nstruct)
 
-    print gr_sorted
-    print gr
+
     for sym in gr_sorted:
         if gr.is_constant(sym):
             continue
+
         if sym not in gr.adj.keys():
             continue
+
         op = gr.adj[sym]
+        if sym in gr.uniques:
+            continue
+
         if op is not None:
             sym_prop = gr.properties[sym]
-
-            # if sym_prop['type'] == 'group':
-                # continue
-
             decl = "const {type} {name}".format(
                 type=to_cpp_type(sym_prop),
                 name=sym
@@ -151,9 +197,10 @@ def express(gr):
 
 def test_graph():
     import example_graphs
-    gr = example_graphs.double_integrator()
+    # gr = example_graphs.double_integrator()
     # gr = example_graphs.simple_graph()
-    # gr = example_graphs.rotary_double_integrator()
+    gr = example_graphs.rotary_double_integrator()
+    # gr = example_graphs.controlled_vectorspring()
 
     return gr
 
