995761810e32ed754e8ef9134c2b5b1ef7828ad9
Jacob Panikulam <jpanikul@gmail.com>
2018-12-01 14:21:57 -0500

    Notes on tensors

--- op_graph/tensors.py
+++ op_graph/tensors.py
@@ -17,6 +17,58 @@ def einsum(gr, a, b):
     pass
 
 
+"""
+Notes:
+    - Clear distinction between vectors and covectors may be unnecessary?
+    - Could treat everything as a tensor of rank (n, n)
+        - And then two-forms/bilinear forms are just matrices
+
+vector: vec(a)
+matrix: (cov(a), vec(b))
+
+bilinear form: (vec(a), vec(b))
+two-form: (cov(a), cov(b))
+
+>>> a = Tensor([3])
+>>> b = Tensor([3])
+>>> c = rmul(a, b)
+    -> Tensor([3])
+    == (a^k * b^k) -> c^k
+
+
+>>> c = Tensor([], [3])
+>>> d = rmul(a, c)
+    -> Scalar()
+    == (a^k * c_k) -> d
+
+>>> D = Tensor([3], [3])
+>>> E = Tensor([3], [3])
+>>> F = rmul(D, E)
+    -> Tensor([3], [3])
+    == (D^a_b * E^b_c) -> F^a_c
+
+(Am I applying rank correctly here?)
+>>> A = Tensor([], [3, 3])
+>>> x = Tensor([3])
+>>> g = rmul(A, x)
+    -> g = Tensor([], [3])
+    == (A_ab * x^b) -> g_a
+>>> c = rmul(x, g)
+    -> c = Scalar()
+    == (g_a * x^a) -> c
+
+dc/dx^a = (dc/dg_b * dg^b/dx_a) + dc/dx^a
+    dc/dg = x^a
+    dg_a/dx^b = A_ab
+    dc/dx^a = g_a
+
+    dc/dx^a = (A_ab * x^a) + g_a
+     = (A_ab * x^a) + A_ab * x^a
+     = 2 * A_ab * x^a
+
+"""
+
+
 ricci_gemm = {
     ('matrix', 'matrix'): {
         # Add a function for curring special arguments
