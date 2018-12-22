5eec872a22e3eb95099dbc603b1fe101095ca791
Jacob Panikulam <jpanikul@gmail.com>
2018-12-22 02:37:29 -0500

    Update jet ekf with adjoint

--- examples/generate_jet_kf.py
+++ examples/generate_jet_kf.py
@@ -7,7 +7,32 @@ from op_graph import groups
 
 def imu_observation_model():
     gr = graph.OpGraph()
-    gr.vector('eps_dot', 6)
+    world_from_sensor = gr.se3('T_world_from_sensor')
+
+    eps_dot = gr.vector('eps_dot', 6)
+    eps_ddot = gr.vector('eps_ddot', 6)
+
+    w = gr.block('w', eps_dot, 0, 0, 3, 1)
+    v = gr.block('v', eps_dot, 3, 0, 3, 1)
+
+    a = gr.block('a', eps_ddot, 0, 0, 3, 1)
+    q = gr.block('q', eps_ddot, 3, 0, 3, 1)
+
+    adj = gr.adjoint('adj', world_from_sensor)
+    accel_terms = gr.mul('accel_terms', adj, 'eps_ddot')
+
+    R = gr.block('R', adj, 0, 0, 3, 3)
+    Rvx = gr.cross_matrix('Rvx', gr.mul(gr.anon(), R, v))
+    Rvxw = gr.mul(gr.anon(), Rvx, w)
+
+    centrifugal = gr.cross(gr.anon(), )
+
+    velocity_terms =
+
+    gr.block('oblock', prod, 0, 0, 3, 1)
+
+    print gr
+    return gr
 
 
 def make_jet():
@@ -23,6 +48,9 @@ def make_jet():
 
 
 def main():
+    imu_observation_model()
+    exit(0)
+
     jet_graph = make_jet()
     print jet_graph
     rk4 = integration.rk4_integrate_no_control(jet_graph)
